f7bf90f9f48941999e308d1a0436bea031641200
bgravesteijn <b.gravesteijn@erasmusmc.nl>
2019-11-04 09:56:07 +0100

    Update mean_impute.R

--- R/mean_impute.R
+++ R/mean_impute.R
@@ -7,7 +7,7 @@ mean_impute <- function(data=NULL){
     if(sum(is.na(data[,i]))!=0){
       if(class(data[,i])=="factor"){
         mode_val <- names(which(table(data[,i])==max(table(data[,i]))))
-        meani[,i][is.na(data[,i])] <- mode_val
+        data[,i][is.na(data[,i])] <- mode_val
       }else{
         mean_val <- median(data[,i], na.rm=TRUE)
         data[,i][is.na(data[,i])] <- mean_val
